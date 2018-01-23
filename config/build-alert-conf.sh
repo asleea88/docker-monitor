a7bc7dd04ba6e9a932ea664bffe4e7d98becca51
SangminKim <asleea88@gamil.com>
2018-01-24 00:00:17 +0900

    Add checking if EMAIL_ADDR and EAMIL_PASSWD variable are set

--- config/build-alert-conf.sh
+++ config/build-alert-conf.sh
@@ -4,6 +4,12 @@ if [ ! -f alertmanager.yml.template ]; then
     echo "ERROR: No alertmanager.yml.template"
 fi
 
+if [ -z $EMAIL_ADDR ] || [ -z $EMAIL_PASSWD ]; then
+    echo "# No EMAIL_ADDR or EMAIL_PASSWD variable"
+    exit
+fi
+
+
 conf_format="$(cat alertmanager.yml.template)"
 
 eval conf=\""${conf_format}"\"
